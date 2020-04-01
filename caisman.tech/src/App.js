1abf3fdf31add891bb224b912bda33f295dba167
Caisman <caiussuciu@gmail.com>
2020-04-01 16:15:16 +0300

    again

--- caisman.tech/src/App.js
+++ caisman.tech/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "./static/footer.css";
-import "./static/common.css";
 import Menu from "./components/Menu/Menu";
 import Footer from "./components/Footer/Footer";
 
