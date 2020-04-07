2ff711633854d71b1dd25425ed6a4fb750c22b4a
Caisman <caiussuciu@gmail.com>
2020-04-07 15:50:07 +0300

    conditional rendering based on screen size

--- caisman.tech/src/components/Menu/Menu.js
+++ caisman.tech/src/components/Menu/Menu.js
@@ -8,41 +8,69 @@ import Subscribe from '../Subscribe/Subscribe';
 import Contact from '../Contact/Contact';
 
 class Menu extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <nav>
-            <div className="menu-center">
-              <ul className="menu-list">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/resume">Resume</Link>
-                </li>
-                <li>
-                  <Link to="/subscribe">Subscribe</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: false,
+    };
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
 
-          <Switch>
-            <Route path="/about" component={About}></Route>
-            <Route path="/resume" component={Resume}></Route>
-            <Route path="/subscribe" component={Subscribe}></Route>
-            <Route path="/contact" component={Contact}></Route>
-            <Route path="/" component={Home}></Route>
-          </Switch>
-        </div>
-      </Router>
+  componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener('resize', this.updatePredicate);
+  }
+
+  componentWillMount() {
+    window.removeEventListener('resize', this.updatePredicate);
+  }
+
+  updatePredicate() {
+    this.setState({ isMobile: window.innerWidth < 768 });
+  }
+
+  render() {
+    const isMobile = this.state.isMobile;
+    return (
+      <div>
+        {isMobile ? (
+          <div>Heloo there</div>
+        ) : (
+          <Router>
+            <div>
+              <nav>
+                <div className="menu-center">
+                  <ul className="menu-list">
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                      <Link to="/resume">Resume</Link>
+                    </li>
+                    <li>
+                      <Link to="/subscribe">Subscribe</Link>
+                    </li>
+                    <li>
+                      <Link to="/contact">Contact</Link>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+
+              <Switch>
+                <Route path="/about" component={About}></Route>
+                <Route path="/resume" component={Resume}></Route>
+                <Route path="/subscribe" component={Subscribe}></Route>
+                <Route path="/contact" component={Contact}></Route>
+                <Route path="/" component={Home}></Route>
+              </Switch>
+            </div>
+          </Router>
+        )}
+      </div>
     );
   }
 }
