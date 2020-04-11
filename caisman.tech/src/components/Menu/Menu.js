4bd6c12b58e4b438f30e8f5b43474f88de2f652a
Caisman <caiussuciu@gmail.com>
2020-04-11 19:46:39 +0300

    plm

--- caisman.tech/src/components/Menu/Menu.js
+++ caisman.tech/src/components/Menu/Menu.js
@@ -13,10 +13,13 @@ class Menu extends Component {
     this.state = {
       isMobile: false,
     };
+
     this.updatePredicate = () => {
       this.setState({ isMobile: window.innerWidth < 768 });
     };
   }
+
+  openMenu = () => {};
 
   componentDidMount() {
     this.updatePredicate();
@@ -30,43 +33,68 @@ class Menu extends Component {
   render() {
     const isMobile = this.state.isMobile;
     return (
-      <Router>
-        <div>
-          <nav>
-            <div className="menu-center">
-              {isMobile ? (
-                <div>Heloo there</div>
-              ) : (
-                <ul className="menu-list">
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/about">About</Link>
-                  </li>
-                  <li>
-                    <Link to="/resume">Resume</Link>
-                  </li>
-                  <li>
-                    <Link to="/subscribe">Subscribe</Link>
-                  </li>
-                  <li>
-                    <Link to="/contact">Contact</Link>
-                  </li>
-                </ul>
-              )}
-            </div>
-          </nav>
+      <div className="header-menu">
+        <Router>
+          <div className="container">
+            <div className="tie-alightleft">
+              <nav>
+                <div className="menu-center">
+                  {isMobile ? (
+                    <ul className="mobile-header">
+                      <Link to="/">
+                        <img
+                          onClick={this.openMenu}
+                          className="first-img"
+                          src={require('../../static/icon.svg')}
+                          alt=""
+                        ></img>
+                      </Link>
 
-          <Switch>
-            <Route path="/about" component={About}></Route>
-            <Route path="/resume" component={Resume}></Route>
-            <Route path="/subscribe" component={Subscribe}></Route>
-            <Route path="/contact" component={Contact}></Route>
-            <Route path="/" component={Home}></Route>
-          </Switch>
-        </div>
-      </Router>
+                      <img
+                        className="rightImage"
+                        src={require('../../static/bars.svg')}
+                        alt=""
+                      ></img>
+                    </ul>
+                  ) : (
+                    <ul className="menu-list">
+                      <li>
+                        <Link to="/">
+                          <img
+                            className="desktopImage"
+                            src={require('../../static/icon.svg')}
+                            alt=""
+                          ></img>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/about">About</Link>
+                      </li>
+                      <li>
+                        <Link to="/resume">Resume</Link>
+                      </li>
+                      <li>
+                        <Link to="/subscribe">Subscribe</Link>
+                      </li>
+                      <li>
+                        <Link to="/contact">Contact</Link>
+                      </li>
+                    </ul>
+                  )}
+                </div>
+              </nav>
+
+              <Switch>
+                <Route path="/about" component={About}></Route>
+                <Route path="/resume" component={Resume}></Route>
+                <Route path="/subscribe" component={Subscribe}></Route>
+                <Route path="/contact" component={Contact}></Route>
+                <Route path="/" component={Home}></Route>
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </div>
     );
   }
 }
