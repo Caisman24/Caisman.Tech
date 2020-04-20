e93065bdc37e419871e5c94d4b732dddd0e282bb
Caisman <caiussuciu@gmail.com>
2020-04-20 14:32:37 +0300

    Refactor menu and app

--- caisman.tech/src/components/About/About.js
+++ caisman.tech/src/components/About/About.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import Menu from '../Menu/Menu';
+import Footer from '../Footer/Footer';
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.menuRef = React.createRef();
+  }
+
   render() {
-    return <div>On about page</div>;
+    return (
+      <div className="content-on-page">
+        <Menu ref={this.menuRef}></Menu>
+        <div>On about page</div>
+        <Footer></Footer>
+      </div>
+    );
   }
 }
 
--- caisman.tech/src/components/Home/Home.js
+++ caisman.tech/src/components/Home/Home.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import Menu from '../Menu/Menu';
+import Footer from '../Footer/Footer';
+
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.menuRef = React.createRef();
+  }
+
   render() {
-    return <div>Home page</div>;
+    return (
+      <div className="content-on-page">
+        <Menu ref={this.menuRef}></Menu>
+        <div>Home page</div>
+        <Footer></Footer>
+      </div>
+    );
   }
 }
 
